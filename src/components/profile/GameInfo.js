93e5048388aca6ae8affc9350481554a09b6be0a
bropy <bohdandesna22@gmail.com>
2024-08-19 14:42:44 +0300

    deploy problems solved

--- src/components/profile/GameInfo.js
+++ src/components/profile/GameInfo.js
@@ -1,7 +1,7 @@
+import React from 'react';
 import text from '../../styles/Text.module.css';
 import positioning from '../../styles/Positioning.module.css';
 import styles from '../../styles/profile/GameInfo.module.css';
-
 
 export default function GameInfo({game}) {
     if (!game) {
@@ -10,12 +10,22 @@ export default function GameInfo({game}) {
 
     const { id, title = 'Untitled Game', time = 'N/A', lastPlay = 'N/A', achivements } = game;
 
+    const handleTitleClick = () => {
+        window.location.href = `/app/${id}`;
+    };
+
+    const handleAchievementsClick = () => {
+        window.location.href = `/app/${id}/achivements`;
+    };
+
     return (
         <div className={`${positioning.row} ${styles.gameInfo}`}>
             <div className={styles.image} alt={`${title} thumbnail`} />
             <div className={`${positioning.column} ${styles.info}`}>
-                <div className={`${text.textMediumSmall} ${text.fontWidth800} ${styles.interactive}`}
-                    onClick={() => window.location.href = `/app/${id}`}>
+                <div 
+                    className={`${text.textMediumSmall} ${text.fontWidth800} ${styles.interactive}`}
+                    onClick={handleTitleClick}
+                >
                     {title}
                 </div>
                 <br />
@@ -28,17 +38,21 @@ export default function GameInfo({game}) {
                     <div> 
                         <div className={styles.line} />
                         <div className={`${positioning.row} ${positioning.justifyBetween} ${positioning.alignCenter}`}>
-                            <div className={styles.interactive}
-                                onClick={() => window.location.href = `/app/${id}/achivements`}>
-                                {`Здобуття досягнень  ${achivements.unlocked} з ${achivements.total}`}
+                            <div 
+                                className={styles.interactive}
+                                onClick={handleAchievementsClick}
+                            >
+                                {`Здобуття досягнень  ${achivements.unlocked} з ${achivements.total}`}
                             </div>
-                            <progress value={achivements.unlocked} 
+                            <progress 
+                                value={achivements.unlocked} 
                                 max={achivements.total} 
-                                className={styles.progress} />
+                                className={styles.progress} 
+                            />
                         </div>
                     </div>
                 )}
             </div>
         </div>
     );
-};
+}
